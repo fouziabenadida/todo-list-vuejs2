2db3fb7b80c00f7ef311d2835c144e1f9dcc5c76
fouziabenadida <benadiafouzia@gmail.com>
2022-08-21 14:46:12 +0300

    first commit

---
+++ todo-list/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+  <header-todo></header-todo>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+   'header-todo': Header
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ todo-list/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+  <b-navbar type="light" variant="dark">
+   <h1 style="color:beige; text-transform: uppercase" >To Do List</h1>
+  </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'header-todo',
+    data() {
+        return{
+
+        }
+    }
+}
+
+
+</script>
+
+
+<style scoped>
+
+</style>
