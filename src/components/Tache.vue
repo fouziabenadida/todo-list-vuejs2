fb62a4215a9de43ec782752d7d092d3030fc8bfa
fouziabenadida <benadiafouzia@gmail.com>
2022-08-24 13:08:41 +0300

    refactor

--- src/components/Tache.vue
+++ src/components/Tache.vue
@@ -1,61 +1,65 @@
 <template>
-   <div class="container mt-5">
-    <h1 style="textAlign:center">Rentrez les tâches à faire. </h1>
-    <form >
-    <div class="form-group">
-  <label for="action">Action</label>
-  <input v-model="formData.tache" type="text" id="action" class="form-control">
-    </div>
-    <button @click.prevent="createTache" class="btn btn-primary mt-4 mb-4" >Créer une tâche </button>
-
+  <div class="container mt-5">
+    <h1 style="textalign: center">Rentrez les tâches à faire.</h1>
+    <form>
+      <div class="form-group">
+        <label for="action">Action</label>
+        <input
+          v-model="formData.tache"
+          type="text"
+          id="action"
+          class="form-control"
+        />
+      </div>
+      <button @click.prevent="createTache" class="btn btn-primary mt-4 mb-4">
+        Créer une tâche
+      </button>
     </form>
-  
-    <ul>
-        <li v-bind:key="index" v-for="(tache,index) in tacheTable">
-        <item-vue v-bind:key="index" :tache="tache" :suppression="suppression"></item-vue>
-        </li>
-    </ul>
 
-   </div>
+    <ul>
+      <li v-bind:key="index" v-for="(tache, index) in tacheTable">
+        <item-vue
+          v-bind:key="index"
+          :tache="tache"
+          :suppression="() => suppression(index)"
+        ></item-vue>
+      </li>
+    </ul>
+  </div>
 </template>
 
 
 <script>
-import Item from './Item'
-export default{
-    name: 'tache-a-faire',
-    data() {
-        return{
-          formData:{
-            tache:"",
-          },
-          tacheTable:['React','Vue','Javascript','Tailwind']
-          
-        }
-        
+import Item from "./Item";
+export default {
+  name: "tache-a-faire",
+  data() {
+    return {
+      formData: {
+        tache: "",
+      },
+      tacheTable: ["React", "Vue", "Javascript", "Tailwind"],
+    };
+  },
+  components: {
+    "item-vue": Item,
+  },
+  methods: {
+    createTache: function () {
+      this.tacheTable.push(this.formData.tache);
+      this.formData.tache = "";
     },
-    components:{
-        'item-vue': Item
+    suppression: function (index) {
+      this.tacheTable.splice(index, 1);
     },
-    methods:{
-        createTache: function(){
-            this.tacheTable.push(this.formData.tache)
-            this.formData.tache= ''
-        },
-        suppression: function(e){
-            this.tacheTable.splice(e.target.parentNode.id, 1)
-        }
-
-    }
-    
-}
-
+  },
+};
 </script>
 
 
 <style scoped>
-ul{
-    list-style-type: none;
-    padding: 0;
+ul {
+  list-style-type: none;
+  padding: 0;
 }
 </style>
